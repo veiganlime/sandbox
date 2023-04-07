a0eb1be987e5ec2316b90306dbfe93e484c17357
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-04-07 15:44:44 +0200

    fuctions and errors

--- Database application/main.py
+++ Database application/main.py
@@ -43,73 +43,81 @@ def edit():
     conn.close()
 
     editor_window.destroy()
-
-
 #Creating function update
 def update():
-    global editor_window
-    editor_window = Tk()
-    editor_window.title('Bearbeitung')
-    editor_window.geometry("400x600")
+    try:
+        int(delete_box.get())
+        error_antwort_lable.config(text="")
+        global editor_window
+        editor_window = Tk()
+        editor_window.title('Bearbeitung')
+        editor_window.geometry("400x300")
 
-    # Creating a database or connecting
-    conn = sqlite3.connect('coins_db.db')
-    # Creating cursor
-    c = conn.cursor()
+        # Creating a database or connecting
+        conn = sqlite3.connect('coins_db.db')
+        # Creating cursor
+        c = conn.cursor()
 
-    # Select from table
-    eintrag_ID = delete_box.get()
-    c.execute("SELECT * FROM coins WHERE oid = " + eintrag_ID)
-    eintraege = c.fetchall()
+        # Select from table
+        eintrag_ID = delete_box.get()
+        c.execute("SELECT * FROM coins WHERE oid = " + eintrag_ID)
+        eintraege = c.fetchall()
 
-    # Creating Global Variables for text box name
-    global name_editor
-    global price_editor
-    global anzahl_editor
+        # Creating Global Variables for text box name
+        global name_editor
+        global price_editor
+        global anzahl_editor
 
-    # Creating Text Boxes
-    name_editor = Entry(editor_window, width=30)
-    name_editor.grid(row=0, column=1, padx=20)
-    price_editor = Entry(editor_window, width=30)
-    price_editor.grid(row=1, column=1, padx=20)
-    anzahl_editor = Entry(editor_window, width=30)
-    anzahl_editor.grid(row=2, column=1, padx=20)
+        # Creating Text Boxes
+        name_editor = Entry(editor_window, width=30)
+        name_editor.grid(row=0, column=1, padx=20)
+        price_editor = Entry(editor_window, width=30)
+        price_editor.grid(row=1, column=1, padx=20)
+        anzahl_editor = Entry(editor_window, width=30)
+        anzahl_editor.grid(row=2, column=1, padx=20)
 
-    # Creating text box labels
-    name_editor_lable = Label(editor_window, text="Coin")
-    name_editor_lable.grid(row=0, column=0)
-    price_editor_lable = Label(editor_window, text="Price")
-    price_editor_lable.grid(row=1, column=0)
-    anzahl_editor_lable = Label(editor_window, text="Anzahl")
-    anzahl_editor_lable.grid(row=2, column=0)
+        # Creating text box labels
+        name_editor_lable = Label(editor_window, text="Coin")
+        name_editor_lable.grid(row=0, column=0)
+        price_editor_lable = Label(editor_window, text="Price")
+        price_editor_lable.grid(row=1, column=0)
+        anzahl_editor_lable = Label(editor_window, text="Anzahl")
+        anzahl_editor_lable.grid(row=2, column=0)
 
-    # loop thru results
-    for eintrag in eintraege:
-        name_editor.insert(0, eintrag[0])
-        price_editor.insert(0, eintrag[1])
-        anzahl_editor.insert(0, eintrag[2])
+        # loop throught results
+        for eintrag in eintraege:
+            name_editor.insert(0, eintrag[0])
+            price_editor.insert(0, eintrag[1])
+            anzahl_editor.insert(0, eintrag[2])
 
-    # Creating button Submit
-    submit_editor_btn = Button(editor_window, text="Eintrag hinzufügen", command=edit)
-    submit_editor_btn.grid(row=3, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+        # Creating button Submit
+        submit_editor_btn = Button(editor_window, text="Eintrag hinzufügen", command=edit, width=50)
+        submit_editor_btn.grid(row=3, column=0, columnspan=2, pady=10, padx=10)
 
-    # Commiting changes
-    conn.commit()
-    # close Connection
-    conn.close()
+        # Commiting changes
+        conn.commit()
+        # close Connection
+        conn.close()
+
+    except ValueError:
+        error_antwort_lable.config(text="Entry value isn't correct", fg="red")
 #Creating function delete
 def delete():
-    # Creating a database or connecting
-    conn = sqlite3.connect('coins_db.db')
-    # Creating cursor
-    c = conn.cursor()
-    # Delete from table
-    c.execute("DELETE from coins WHERE oid = " + delete_box.get())
+    try:
+        int(delete_box.get())
+        # Creating a database or connecting
+        conn = sqlite3.connect('coins_db.db')
+        # Creating cursor
+        c = conn.cursor()
+        # Delete from table
+        c.execute("DELETE from coins WHERE oid = " + delete_box.get())
 
-    # Commiting changes
-    conn.commit()
-    # close Connection
-    conn.close()
+        # Commiting changes
+        conn.commit()
+        # close Connection
+        conn.close()
+    except ValueError:
+        error_antwort_lable.config(text="Entry value isn't correct", fg="red")
 #Creating function query
 def query():
     # Creating a database or connecting
@@ -134,27 +142,39 @@ def query():
     conn.close()
 #Creating function Submit
 def submit():
-    # Creating a database or connecting
-    conn = sqlite3.connect('coins_db.db')
-    # Creating cursor
-    c = conn.cursor()
-    #Insert into table
-    c.execute("INSERT INTO coins VALUES(:name,:price,:anzahl)",
-              {
-                  'name' : name.get(),
-                  'price' : price.get(),
-                  'anzahl' : anzahl.get()
-              })
+    try:
+        if not name.get():
+            raise ValueError
+        if not price.get():
+            raise ValueError
+        if not anzahl.get():
+            raise ValueError
+    except ValueError:
+        error_antwort_lable.config(text="Entry value isn't correct", fg="red")
+    else:
+        error_antwort_lable.config(text="")
+        # Creating a database or connecting
+        conn = sqlite3.connect('coins_db.db')
+        # Creating cursor
+        c = conn.cursor()
+        # Insert into table
+        c.execute("INSERT INTO coins VALUES(:name,:price,:anzahl)",
+                  {
+                      'name': name.get(),
+                      'price': price.get(),
+                      'anzahl': anzahl.get()
+                  })
 
-    #Clear the text boxes
-    name.delete(0, END)
-    price.delete(0, END)
-    anzahl.delete(0, END)
+        # Clear the text boxes
+        name.delete(0, END)
+        price.delete(0, END)
+        anzahl.delete(0, END)
 
-    # Commiting changes
-    conn.commit()
-    # close Connection
-    conn.close()
+        # Commiting changes
+        conn.commit()
+        # close Connection
+        conn.close()
+
 # Creating Text Boxes
 name = Entry(root, width=30)
 name.grid(row=0, column=1, padx=20)
@@ -174,22 +194,24 @@ anzahl_lable = Label(root, text="Anzahl")
 anzahl_lable.grid(row=2, column=0)
 delete_box_label = Label(root, text="ID zu bearbeiten")
 delete_box_label.grid(row=7,column=0)
+error_antwort_lable = Label(root, text= '')
+error_antwort_lable.grid(row=9,column=0)
 
 # Creating button Submit
-submit_btn = Button(root, text="Eintrag hinzufügen", command=submit)
-submit_btn.grid(row=3, column=0, columnspan=2,pady=10, padx=10, ipadx=100)
+submit_btn = Button(root, text="Eintrag hinzufügen", command=submit, width=50)
+submit_btn.grid(row=3, column=0, columnspan=2,pady=10, padx=10)
 
 #Creating a query button
-query_btn = Button(root, text="Alle Einträge anzeigen", command=query)
-query_btn.grid(row=4, column=0, columnspan=2,pady=10, padx=10, ipadx=93)
+query_btn = Button(root, text="Alle Einträge anzeigen", command=query, width=50)
+query_btn.grid(row=4, column=0, columnspan=2,pady=10, padx=10)
 
 #Creating a delete button
-delte_btn = Button(root, text="Eintrag löschen", command=delete)
-delte_btn.grid(row=6, column=0, columnspan=2,pady=10, padx=10, ipadx=110)
+delte_btn = Button(root, text="Eintrag löschen", command=delete, width=50)
+delte_btn.grid(row=6, column=0, columnspan=2,pady=10, padx=10)
 
 #Creating a update button
-update_btn = Button(root, text="Eintrag bearbeiten", command=update)
-update_btn.grid(row=8, column=0, columnspan=2,pady=10, padx=10, ipadx=110)
+update_btn = Button(root, text="Eintrag bearbeiten", command=update, width=50)
+update_btn.grid(row=8, column=0, columnspan=2,pady=10, padx=10)
 #Commiting changes
 conn.commit()
 #close Connection
